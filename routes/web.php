ffbdcfc699fe617c72f803d00bd5caac57473698
darrenz1212 <113487535+darrenz1212@users.noreply.github.com>
2024-11-01 14:12:38 +0700

    adding adminLTE for admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts/starter');
 });
 
 Route::get('/dashboard', function () {
