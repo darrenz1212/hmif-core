f572c8c1654c47d54631de2dbf1634f3fec8efa2
JohanesMarioPL <113487294+JohanesMarioPL@users.noreply.github.com>
2025-01-16 21:54:44 +0700

    fix-ui

--- database/seeders/HmifSeeder.php
+++ database/seeders/HmifSeeder.php
@@ -16,11 +16,11 @@ class HmifSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'name' => 'HMIF User',
-            'email' => 'hmif@example.com',
+            'name' => 'staff User',
+            'email' => 'staff@example.com',
             'email_verified_at' => now(),
             'password' => Hash::make('password'), // Ganti dengan password yang diinginkan
-            'role' => 'hmif', // Tambahkan kolom 'role' di tabel jika belum ada
+            'role' => 'stafflab', // Tambahkan kolom 'role' di tabel jika belum ada
             'created_at' => now(),
             'updated_at' => now(),
         ]);
